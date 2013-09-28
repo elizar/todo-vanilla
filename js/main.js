93e68bdbac2a4c2ce1da1aec5624df5d6fbcd420
Elizar Pepino <jupenz@gmail.com>
2013-09-28 15:41:35 +0800

    done

--- js/main.js
+++ js/main.js
@@ -4,6 +4,22 @@
 
   function init() {
     // Start everything here...
+    var form = document.getElementById('form'),
+        input = form.querySelector('#todo'),
+        list = document.getElementById('todo-lists');
+
+    form.onsubmit = function(e) {
+
+      e.preventDefault();
+      // TODO: add new tod
+      var newTodo = input.value.trim();
+      var li = document.createElement('li');
+          li.innerText = newTodo;
+      list.appendChild(li);
+      input.value = '';
+      input.focus();
+
+    };
   }
 
   window.onload = function() {
