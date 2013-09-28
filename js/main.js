c59bddeb2647b4fae02aeb82d56ed8f3058841f4
Elizar Pepino <jupenz@gmail.com>
2013-09-28 14:50:47 +0800

    updated main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function() {
+
+  'use strict';
+
+  function init() {
+    // Start everything here...
+  }
+
+  window.onload = function() {
+    if (document.readyState === 'complete') {
+      init();
+    }
+  };
+})();
